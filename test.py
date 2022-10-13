d9c497e140f6aa0c65a2ae898428adcf8ba01c3e
Sampark Sharma <phantsure@github.com>
2022-10-13 11:51:26 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,6 @@ print(ACTIONS_RUNTIME_TOKEN)
 
 URL = "https://artifactcache.actions.githubusercontent.com/NHtGqffqobkG22BvxkwjZ50krG053v2tfi839LARA7jZONk0ux/_apis/artifactcache/cache?keys=Linux-3182001628&version=db813e27660a005fc99305e68352e9609af9eb801d1200177174a28cce3a7bc9"
 
-response = requests.get(url = URL, {"Authorization": "Bearer " + ACTIONS_RUNTIME_TOKEN})
+response = requests.get(url = URL, headers = {"Authorization": "Bearer " + ACTIONS_RUNTIME_TOKEN})
 
 print(response.json())
