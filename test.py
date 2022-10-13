4e8cec43646fe8fde9125b552a83f5fa43eff8c9
Sampark Sharma <phantsure@github.com>
2022-10-13 12:22:24 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import os
 ACTIONS_RUNTIME_TOKEN = os.environ.get("ACTIONS_RUNTIME_TOKEN")
 print(ACTIONS_RUNTIME_TOKEN)
 
-URL = "https://artifactcache.actions.githubusercontent.com/NHtGqffqobkG22BvxkwjZ50krG053v2tfi839LARA7jZONk0ux/_apis/artifactcache/cache?keys=Linux-3182001628&version=db813e27660a005fc99305e68352e9609af9eb801d1200177174a28cce3a7bc9"
+URL = "https://artifactcache.actions.githubusercontent.com/NHtGqffqobkG22BvxkwjZ50krG053v2tfi839LARA7jZONk0ux/_apis/artifactcache/cache?keys=cache-3240252986&version=3a594552ed0aa82f373e587ff9934771fc4bb57cccb5172b10b40979e8e9d248"
 
 response = requests.get(url = URL, headers = {"Authorization": "Bearer " + ACTIONS_RUNTIME_TOKEN})
 
