93ecb473794f1c4b5bc5128a172ae8cee86097d6
Sampark Sharma <phantsure@github.com>
2022-10-13 12:53:40 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,11 +2,10 @@ import requests
 import os
 
 ACTIONS_RUNTIME_TOKEN = os.environ.get("ACTIONS_RUNTIME_TOKEN")
-print(ACTIONS_RUNTIME_TOKEN)
+CACHE_URL = os.environ.get("CACHE_URL")
+print(CACHE_URL)
 
-URL = "https://artifactcache.actions.githubusercontent.com/NHtGqffqobkG22BvxkwjZ50krG053v2tfi839LARA7jZONk0ux/_apis/artifactcache/cache?keys=cache-3240252986&version=3a594552ed0aa82f373e587ff9934771fc4bb57cccb5172b10b40979e8e9d248"
-
-response = requests.get(url = URL, headers = {"Authorization": "Bearer " + ACTIONS_RUNTIME_TOKEN})
+response = requests.get(url = CACHE_URL, headers = {"Authorization": "Bearer " + ACTIONS_RUNTIME_TOKEN})
 
 print(response.json())
 
