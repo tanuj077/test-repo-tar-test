9045c413df0ea61d5fef933a7d34cfed582ef189
Sampark Sharma <phantsure@github.com>
2022-11-11 16:35:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ print(CACHE_URL)
 
 response = requests.get(url = CACHE_URL, headers = {"Authorization": "Bearer " + ACTIONS_RUNTIME_TOKEN})
 
-print(response.json())
+print(response.text, response.json())
 
 archiveLocation = response.json()["archiveLocation"]
 
